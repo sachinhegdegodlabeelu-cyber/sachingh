d6c4118c75e4478234530c364786cd17b97c44c3
sachinhegdegodlabeelu-cyber <sachinhegdegodlabeelu@gmail.com>
2025-10-17 19:20:46 +0530

    Add perimeter calculation for a rectange
    Initial commit with perimeter calculation for a rectangle.

---
+++ sachingh.py
@@ -0,0 +1,13 @@
+print("initial commit")
+print("--------S----A---C----H----I-----N----------************G----I-------R------I---------S--------H---*****************H----E---------G-------D-------E")
+def perimeterofsquare(length,breadth):
+  #formula  ==2(l)+2(b)===2(l+b)
+  perimeter = 2*length+breadth
+  print(f"perimeter length in total =={perimeter}")
+  return perimeeter
+
+
+length=4
+breadth=9
+perimeterofsquare(length,breadth)
+print("calculation done")
